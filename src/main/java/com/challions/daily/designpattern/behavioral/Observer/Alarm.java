39de36262730dfbfa6342899d2701d2d3f21e116
Joseph千橙 <mgr.qiancheng@gmail.com>
2016-05-05 23:55:15 +0800

    Observer Pattern

---
+++ src/main/java/com/challions/daily/designpattern/behavioral/Observer/Alarm.java
@@ -0,0 +1,13 @@
+package com.challions.daily.designpattern.behavioral.Observer;
+
+import java.util.Observable;
+import java.util.Observer;
+
+/**
+ * Created by iamya on 5/5/2016.
+ */
+public class Alarm implements Observer {
+    public void update(Observable observable, Object o) {
+        System.out.println("Alarm!!! It's BOILLING！ " + o + "°C now!");
+    }
+}
