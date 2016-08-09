d2d742ecfc9f6f8617a1cd5491ba7205867bd9dc
qian-robot <mgr.qiancheng@gmail.com>
2016-08-10 07:48:50 +0800

    2016-08-10 07:48:46 auto push via robot

--- autopush.sh
+++ autopush.sh
@@ -4,7 +4,7 @@ msg=$(date "+  %F %H:%M:%S auto push via robot" )
 echo ${msg}
 git checkout robot
 git pull
-echo -e '- '${msg}'\n' > daily-rolling.md
+echo -e '- '${msg}'\n' >> daily-rolling.md
 git add .
 git commit -m "${msg}"
 git push origin robot
--- daily-rolling.md
+++ daily-rolling.md
@@ -1,2 +1,4 @@
 -  2016-08-10 07:47:54 auto push via robot
 
+-  2016-08-10 07:48:46 auto push via robot
+
